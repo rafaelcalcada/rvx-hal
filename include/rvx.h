650d32f638cec4a13411de692234f329125253c4
Rafael Calcada <rafaelcalcada@gmail.com>
2026-03-15 22:51:08 +0000

    Revision of UART driver API

--- include/rvx.h
+++ include/rvx.h
@@ -1287,63 +1287,60 @@ static inline void rvx_timer_clear_interrupt(RvxTimer *timer_address)
 }
 
 /**
- * @brief Check if the UART is ready to send data.
+ * @brief Return `true` if the UART is ready to transmit a new byte, or `false` otherwise.
  *
  * @note In most cases, you do not need to call this function directly, as UART transmitting
- * functions (e.g., `rvx_uart_write`, `rvx_uart_write_string`) automatically wait until the UART is
+ * functions (e.g., `rvx_uart_send`, `rvx_uart_send_string`) will wait until the UART is
  * ready to transmit.
  *
  * @param uart_address Base address of the UART controller.
  * @return `true` if the UART is ready to send data, `false` otherwise.
  */
-static inline bool rvx_uart_tx_ready(RvxUart *uart_address)
+static inline bool rvx_uart_ready_to_send(RvxUart *uart_address)
 {
   return uart_address->RVX_UART_STATUS_REG & 1;
 }
 
 /**
- * @brief Busy-wait until the UART transmission is complete.
+ * @brief Wait until the UART transmission is complete.
  *
- * This function continuously checks the UART status register and returns only when the UART is ready to
- * send new data.
+ * This function blocks until the UART is ready to send data again.
  *
  * Example usage:
  *
  * ```c *
  * // Send data
- * rvx_uart_write(RVX_UART_ADDRESS, 'A');
+ * rvx_uart_send(RVX_UART_ADDRESS, 'A');
  *
- * // Busy-wait until transmission above is complete
- * rvx_uart_wait_tx_complete(RVX_UART_ADDRESS);
+ * // Wait until transmission above is complete
+ * rvx_uart_wait_tx_done(RVX_UART_ADDRESS);
  * ```
  *
  * @param uart_address Base address of the UART controller.
  */
-static inline void rvx_uart_wait_tx_complete(RvxUart *uart_address)
+static inline void rvx_uart_wait_tx_done(RvxUart *uart_address)
 {
-  while (!rvx_uart_tx_ready(uart_address))
+  while (!rvx_uart_ready_to_send(uart_address))
     ;
 }
 
 /**
- * @brief Check if the UART has received new data.
- *
- * Returns `true` if new data has been received and is waiting to be read, or `false` otherwise.
+ * @brief Return `true` if the UART has received data that has not been read yet, or `false` otherwise.
  *
  * Example usage:
  *
  * ```c
- * // Wait for data to be received (busy-wait)
- * while (!rvx_uart_rx_ready(RVX_UART_ADDRESS));
+ * // Wait for data to be received
+ * while (!rvx_uart_data_available(RVX_UART_ADDRESS));
  *
  * // Read the received byte
- * uint8_t rx_data = rvx_uart_read(RVX_UART_ADDRESS);
+ * uint8_t rx_data = rvx_uart_receive(RVX_UART_ADDRESS);
  * ```
  *
  * @param uart_address Base address of the UART controller.
  * @return `true` if new data is available, `false` if no new data has been received
  */
-static inline bool rvx_uart_rx_ready(RvxUart *uart_address)
+static inline bool rvx_uart_data_available(RvxUart *uart_address)
 {
   return (uart_address->RVX_UART_STATUS_REG >> 1) & 1;
 }
@@ -1354,48 +1351,46 @@ static inline bool rvx_uart_rx_ready(RvxUart *uart_address)
  * This function returns the last byte received by the UART. After power-on or reset, if no data has
  * been received, the function returns `0x00`.
  *
- * To check if new data is available, use `rvx_uart_rx_ready()`.
+ * To check if new data is available, use `rvx_uart_data_available()`.
  *
  * Example usage:
  * ```c
- * // Wait for data to be received (busy-wait)
- * while (!rvx_uart_rx_ready(RVX_UART_ADDRESS));
+ * // Wait for data to be received
+ * while (!rvx_uart_data_available(RVX_UART_ADDRESS));
  *
  * // Read the received byte
- * uint8_t rx_data = rvx_uart_read(RVX_UART_ADDRESS);
+ * uint8_t rx_data = rvx_uart_receive(RVX_UART_ADDRESS);
  * ```
  *
  * @param uart_address Base address of the UART controller.
  * @return The last received byte, or `0x00` if no data has been received since power-on or reset.
  */
-static inline uint8_t rvx_uart_read(RvxUart *uart_address)
+static inline uint8_t rvx_uart_receive(RvxUart *uart_address)
 {
   return uart_address->RVX_UART_READ_REG;
 }
 
 /**
- * @brief Send a single byte over the UART.
+ * @brief Send a byte over the UART.
  *
- * This function busy-waits until the UART is ready before performing the write. There is no
- * need to call `rvx_uart_tx_ready()` or `rvx_uart_wait_tx_complete()` beforehand, as this
- * function handles waiting internally.
+ * Blocks until the UART is idle, then sends a byte.
  *
  * Example usage:
  *
  * ```c
  * // Send 'A' over UART
- * rvx_uart_write(RVX_UART_ADDRESS, 'A');
+ * rvx_uart_send(RVX_UART_ADDRESS, 'A');
  *
  * // Send 'B', busy-waits until 'A' has been fully transmitted
- * rvx_uart_write(RVX_UART_ADDRESS, 'B');
+ * rvx_uart_send(RVX_UART_ADDRESS, 'B');
  * ```
  *
  * @param uart_address Base address of the UART controller.
  * @param data The byte to write (`uint8_t`).
  */
-static inline void rvx_uart_write(RvxUart *uart_address, uint8_t data)
+static inline void rvx_uart_send(RvxUart *uart_address, uint8_t data)
 {
-  rvx_uart_wait_tx_complete(uart_address);
+  rvx_uart_wait_tx_done(uart_address);
   uart_address->RVX_UART_WRITE_REG = data;
 }
 
@@ -1408,17 +1403,17 @@ static inline void rvx_uart_write(RvxUart *uart_address, uint8_t data)
  * Example usage:
  * ```c
  * // Send the string "Hello, World!" over UART, waiting if necessary
- * rvx_uart_write_string(RVX_UART_ADDRESS, "Hello, World!");
+ * rvx_uart_send_string(RVX_UART_ADDRESS, "Hello, World!");
  * ```
  *
  * @param uart_address Base address of the UART controller.
  * @param c_str Pointer to the null-terminated C string to transmit.
  */
-static inline void rvx_uart_write_string(RvxUart *uart_address, const char *c_str)
+static inline void rvx_uart_send_string(RvxUart *uart_address, const char *c_str)
 {
   while (*c_str)
   {
-    rvx_uart_write(uart_address, *c_str++);
+    rvx_uart_send(uart_address, *c_str++);
   }
 }
 
